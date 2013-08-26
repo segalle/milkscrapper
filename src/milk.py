10d360e8b17981bb0a44cee39f8e74f0ed9af857
segalle <erez.segall@gmail.com>
2013-08-26 12:29:05 +0300

    3 first tests resolved

--- src/milk.py
+++ src/milk.py
@@ -1,15 +1,21 @@
 """
 Gets information about Tipat Halav offices
 """
-
+import requests
+from lxml import etree
 
 def get_url(page):
-    pass
+    url = "http://www.health.gov.il/Subjects/vaccines/two_drops/Pages/Vaccination_centers.aspx?WPID=WPQ8&PN=%d" % page
+    return url
 
 
 def get_full_html(url):
-    pass
+    html = requests.get(url)
+    return html.text
 
 
-def extract_stations_table(url):
-    pass
+
+def extract_stations_table(url):    
+    html = etree.HTML(url)
+    table = html.xpath('//*[@class="cqwpGridViewTable cqwpGridViewTableFullVaccines PaymentsGridViewGroup"]')[0];
+    return table
